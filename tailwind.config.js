ad4f328b3b232e172b7dbbaa9c6f5abd9b1d9627
markocic <markokocic2000@gmail.com>
2024-06-01 01:22:42 +0200

    make register page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+            whiteSmoke: '#f3f3f3',
+            yinMnBlue: '#3D5A80',
+            airForceBlue: '#6B8EAD',
+            powderBlue: '#98C1D9',
+            columbiaBlue: '#C6DAE6',
+            burntSienna: '#EE6C4D',
+            gunmetal: '#293241',
+            lapisLazuli: '#49678C',
+            prussianBlue: '#223347',
+            gradientStart: '#243B55',
+            gradientEnd: '#141E30',
+
+            cardBackground: 'rgba(243, 243, 243, 0.05)',
+        },
+        boxShadow: {
+            'card': '0px 4px 14px 3px rgba(0, 0, 0, 0.25)'
+        },
+        
+    },
+    fontFamily: {
+        'sans': ['Questrial'],
+        'body': ['Questrial']
+    }
   },
   plugins: [],
 }
