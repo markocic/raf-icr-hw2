3b81f6fa7d1f71adabab3eb613e5a2f3299e79b5
markocic <markokocic2000@gmail.com>
2024-06-04 15:21:12 +0200

    make chats page responsive

---
+++ js/chats.js
@@ -0,0 +1,35 @@
+let hamburger = document.getElementById("hamburger");
+let chats = document.getElementById("chats")
+
+let toggled = false;
+
+hamburger.addEventListener("click", (e) => {
+    if (toggled)
+        hideSideBar()
+    else 
+        showSideBar()
+
+    toggled = !toggled
+})
+
+const showSideBar = () => {
+    chats.classList.add("absolute")
+    chats.classList.add("w-96")
+    chats.classList.remove("md:w-96")
+    chats.classList.remove("w-0")
+
+    hamburger.classList.remove("left-0")
+    hamburger.classList.add("left-96")
+
+}
+
+const hideSideBar = () => {
+    chats.classList.remove("absolute")
+    chats.classList.remove("w-96")
+    chats.classList.add("md:w-96")
+    chats.classList.add("w-0")
+
+    hamburger.classList.add("left-0")
+    hamburger.classList.remove("left-96")
+
+}
